c0c0c830ba7540775816d5edb2233386ba908429
Nile Jocson <atraphaxiae@gmail.com>
2026-08-05 22:20:19 +0800

    Run cargo fmt

--- src/project.rs
+++ src/project.rs
@@ -9,17 +9,19 @@ use crate::result::Result;
 
 #[derive(Debug)]
 pub struct Project {
-	root: PathBuf
+	root: PathBuf,
 }
 
 impl Project {
 	pub fn init(path: &Path) -> Result<Project, ProjectError> {
-		Ok(Project { root: path.to_path_buf() })
+		Ok(Project {
+			root: path.to_path_buf(),
+		})
 	}
 }
 
 #[derive(Debug, Error)]
 pub enum ProjectError {
 	#[error("Failed to initialize albumctl project at {path:?}")]
-	Init { path: PathBuf }
+	Init { path: PathBuf },
 }
