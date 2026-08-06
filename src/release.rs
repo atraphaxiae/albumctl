d6e844834e1e2c55247666b7a1d30b65df2414bf
Nile Jocson <atraphaxiae@gmail.com>
2026-08-06 23:28:14 +0800

    Add Release struct, implement the same features as Album struct

--- src/release.rs
+++ src/release.rs
@@ -1,9 +1,72 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
-use serde::Deserialize;
+use std::fmt::{Display, Formatter};
+use std::hash::{Hash, Hasher};
+use std::path::{Path, PathBuf};
 
+use error_stack::ResultExt;
+use serde::Deserialize;
+use thiserror::Error;
+
+use crate::filesystem::require_dir;
+use crate::manifest::load_manifest;
+use crate::mapping::MappingManifest;
+use crate::result::Result;
 use crate::tracklist::Disc;
+
+#[derive(Debug)]
+pub struct Release {
+	path: PathBuf,
+	manifest: ReleaseManifest,
+	mapping: MappingManifest,
+}
+
+impl Release {
+	pub fn load(path: &Path) -> Result<Self, ReleaseError> {
+		let error = || ReleaseError::Load {
+			path: path.to_path_buf(),
+		};
+
+		require_dir(path).change_context_lazy(error)?;
+
+		let manifest = path.join("release.toml");
+		let manifest = load_manifest(&manifest).change_context_lazy(error)?;
+
+		let mapping = path.join("mapping.toml");
+		let mapping = load_manifest(&mapping).change_context_lazy(error)?;
+
+		Ok(Release {
+			path: path.to_path_buf(),
+			manifest,
+			mapping,
+		})
+	}
+
+	pub fn path(&self) -> &Path {
+		&self.path
+	}
+}
+
+impl PartialEq for Release {
+	fn eq(&self, other: &Self) -> bool {
+		self.manifest.id == other.manifest.id
+	}
+}
+
+impl Eq for Release {}
+
+impl Hash for Release {
+	fn hash<H: Hasher>(&self, state: &mut H) {
+		self.manifest.id.hash(state);
+	}
+}
+
+impl Display for Release {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		self.manifest.id.fmt(f)
+	}
+}
 
 #[derive(Debug, Deserialize)]
 pub struct ReleaseManifest {
@@ -19,4 +82,25 @@ pub struct ReleaseIdentifier {
 	pub media_type: String,
 	pub audio_channels: String,
 	pub provenance: String,
+}
+
+impl Display for ReleaseIdentifier {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		let Self {
+			catalog_number,
+			media_type,
+			audio_channels,
+			provenance,
+		} = self;
+		write!(
+			f,
+			"{catalog_number}, {media_type}, {audio_channels}, {provenance}"
+		)
+	}
+}
+
+#[derive(Debug, Error)]
+pub enum ReleaseError {
+	#[error("Could not load release at {path:?}")]
+	Load { path: PathBuf },
 }
