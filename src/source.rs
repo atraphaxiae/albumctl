816ef8ba57cb29f24bca2c125e94df66a590711e
Nile Jocson <atraphaxiae@gmail.com>
2026-08-19 21:21:27 +0800

    Add manifest structs

--- src/source.rs
+++ src/source.rs
@@ -1,17 +1,49 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
-#[derive(Debug)]
-pub struct Source {
+use std::path::PathBuf;
 
+use serde::Deserialize;
+
+#[derive(Debug)]
+pub struct Source {}
+
+#[derive(Debug)]
+pub struct AlbumSource {}
+
+#[derive(Debug)]
+pub struct ReleaseSource {}
+
+#[derive(Debug, Deserialize)]
+pub struct ConfigManifest {
+	pub output_dir: PathBuf,
 }
 
-#[derive(Debug)]
-pub struct AlbumSource {
-
+#[derive(Debug, Deserialize)]
+pub struct AlbumManifest {
+	pub artist: String,
+	pub year: u16,
+	pub title: String,
 }
 
-#[derive(Debug)]
-pub struct ReleaseSource {
+#[derive(Debug, Deserialize)]
+pub struct ReleaseManifest {
+	pub year: u16,
+	pub catalog_number: String,
+	pub media_type: String,
+	pub audio_channels: String,
+	pub provenance: String,
 
+	pub discs: Vec<DiscManifest>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct DiscManifest {
+	pub tracks: Vec<TrackManifest>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct TrackManifest {
+	pub title: String,
+	pub file: PathBuf,
 }
