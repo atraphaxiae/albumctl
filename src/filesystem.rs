4fca4d783661c57fe757f4f4f4a13664b30cec2a
Nile Jocson <atraphaxiae@gmail.com>
2026-08-19 20:30:29 +0800

    Add filesystem::list_dirs()

---
+++ src/filesystem.rs
@@ -0,0 +1,49 @@
+// SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
+// SPDX-License-Identifier: MPL-2.0
+
+use std::{
+	io,
+	path::{Path, PathBuf},
+};
+
+use error_stack::ResultExt;
+use thiserror::Error;
+
+use crate::result::Result;
+
+pub fn list_dirs(dir: &Path) -> Result<Vec<PathBuf>, FilesystemError> {
+	let error = || FilesystemError::ListDirs {
+		dir: dir.to_path_buf(),
+	};
+
+	let entries = dir
+		.read_dir()
+		.change_context_lazy(error)
+		.attach_with(|| format!("while reading {dir:?}"))?
+		.collect::<io::Result<Vec<_>>>()
+		.change_context_lazy(error)
+		.attach_with(|| format!("while enumerating entries of {dir:?}"))?;
+
+	let dirs = entries
+		.into_iter()
+		.filter_map(|entry| {
+			let path = entry.path();
+			match entry.file_type() {
+				Ok(file_type) => file_type.is_dir().then_some(Ok(path)),
+				Err(e) => Some(
+					Err(e)
+						.change_context(error())
+						.attach(format!("while reading file type of {path:?}")),
+				),
+			}
+		})
+		.collect::<Result<Vec<_>, _>>()?;
+
+	Ok(dirs)
+}
+
+#[derive(Debug, Error)]
+pub enum FilesystemError {
+	#[error("Could not list the immediate child directories of {dir:?}")]
+	ListDirs { dir: PathBuf },
+}
