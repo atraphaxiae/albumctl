ecbeaf6775a45883ab0aa820ace838e9732d7d8d
Nile Jocson <atraphaxiae@gmail.com>
2026-08-19 17:41:13 +0800

    Start a rewrite from scratch

--- src/result.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
-// SPDX-License-Identifier: MPL-2.0
-
-use error_stack::Report;
-
-pub type Result<T, E> = std::result::Result<T, Report<E>>;
