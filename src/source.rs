5d89bc027ca5952965d031d3bf3a1ca91e3b9d0c
Nile Jocson <atraphaxiae@gmail.com>
2026-08-19 21:40:05 +0800

    Populate the Source structs, add documentation

--- src/source.rs
+++ src/source.rs
@@ -1,18 +1,34 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
+//! Module for loading the source directory into source structures
+//!
+//! Here, we load the manifests referenced by SourceDir into Source. We are not yet concerned with
+//! duplicate album/release detection in this layer.
+
 use std::path::PathBuf;
 
 use serde::Deserialize;
 
 #[derive(Debug)]
-pub struct Source {}
+pub struct Source {
+	pub dir: PathBuf,
+	pub config: ConfigManifest,
+	pub albums: Vec<AlbumSource>,
+}
 
 #[derive(Debug)]
-pub struct AlbumSource {}
+pub struct AlbumSource {
+	pub dir: PathBuf,
+	pub manifest: AlbumManifest,
+	pub releases: Vec<ReleaseSource>,
+}
 
 #[derive(Debug)]
-pub struct ReleaseSource {}
+pub struct ReleaseSource {
+	pub dir: PathBuf,
+	pub manifest: ReleaseManifest,
+}
 
 #[derive(Debug, Deserialize)]
 pub struct ConfigManifest {
