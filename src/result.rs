04f0fd59db42b007878e871319fa3079d713c85f
Nile Jocson <atraphaxiae@gmail.com>
2026-08-19 20:20:13 +0800

    Add result wrapper for error_report::Report

--- src/result.rs
+++ src/result.rs
@@ -1,8 +1,8 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
-mod cli;
-mod result;
-mod tree;
+use std::result;
 
-fn main() {}
+use error_stack::Report;
+
+pub type Result<T, E> = result::Result<T, Report<E>>;
