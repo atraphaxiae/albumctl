c0b8b5531f2ae78bf9c3a6ba6fc6faad19dee42c
Nile Jocson <atraphaxiae@gmail.com>
2026-08-19 22:04:45 +0800

    Add model.rs

--- src/model.rs
+++ src/model.rs
@@ -1,12 +1,2 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
-
-mod cli;
-mod dir;
-mod filesystem;
-mod manifest;
-mod model;
-mod result;
-mod source;
-
-fn main() {}
