51f2148edf80291db92d0c8c07e046b7ece25968
Nile Jocson <atraphaxiae@gmail.com>
2026-08-19 18:00:42 +0800

    Add clap with derive, add cli structure

---
+++ src/cli.rs
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
+// SPDX-License-Identifier: MPL-2.0
+
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+pub struct Cli {
+	#[command(subcommand)]
+	command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Command {
+	Check { dir: PathBuf },
+	Build { dir: PathBuf },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
+mod cli;
+
 fn main() {}
