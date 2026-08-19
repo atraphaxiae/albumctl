f2825d7b969314063d1fb48ff77bcaedfafc556a
Nile Jocson <atraphaxiae@gmail.com>
2026-08-19 20:36:41 +0800

    Add ReleaseTree::from_dir() factory

--- src/tree.rs
+++ src/tree.rs
@@ -1,7 +1,9 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+
+use thiserror::Error;
 
 pub struct SourceTree {
 	dir: PathBuf,
@@ -12,10 +14,28 @@ pub struct SourceTree {
 pub struct AlbumTree {
 	dir: PathBuf,
 	manifest_file: PathBuf,
-	release: Vec<ReleaseTree>,
+	releases: Vec<ReleaseTree>,
 }
 
 pub struct ReleaseTree {
 	dir: PathBuf,
 	manifest_file: PathBuf,
+}
+
+impl ReleaseTree {
+	pub fn from_dir(dir: &Path) -> Self {
+		Self {
+			dir: dir.to_path_buf(),
+			manifest_file: dir.join("release.toml")
+		}
+	}
+}
+
+#[derive(Debug, Error)]
+pub enum TreeError {
+	#[error("Could not create source tree from directory {dir:?}")]
+	SourceTreeFrom { dir: PathBuf },
+
+	#[error("Could not create album tree from directory {dir:?}")]
+	AlbumTreeFrom { dir: PathBuf }
 }
