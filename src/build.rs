e6eaf8692331e2b8f79321ab1347baed18bc16db
Nile Jocson <atraphaxiae@gmail.com>
2026-08-20 20:45:36 +0800

    Add UnitTrack struct

--- src/build.rs
+++ src/build.rs
@@ -1,7 +1,9 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
-use crate::model::Model;
+use std::path::PathBuf;
+
+use crate::model::{DiscInfo, Model, TrackInfo};
 
 pub mod finalize;
 pub mod normalize;
@@ -16,4 +18,12 @@ impl Builder {
 	pub fn new(model: Model) -> Self {
 		Self { model }
 	}
+}
+
+pub struct UnitTrack<'a> {
+	disc_number: u16,
+	track_number: u16,
+	disc_info: &'a DiscInfo,
+	track_info: &'a TrackInfo,
+	file: PathBuf
 }
