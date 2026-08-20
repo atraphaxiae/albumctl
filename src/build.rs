3527a8fe6c7f9ce25e2efd34a14c38d8ae00b79e
Nile Jocson <atraphaxiae@gmail.com>
2026-08-20 16:05:15 +0800

    Add Builder struct

--- src/build.rs
+++ src/build.rs
@@ -1,4 +1,16 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
+use crate::model::Model;
+
 pub mod prepare;
+
+pub struct Builder {
+	model: Model,
+}
+
+impl Builder {
+	pub fn new(model: Model) -> Self {
+		Self { model }
+	}
+}
