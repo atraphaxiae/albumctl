01afe6cad3cdffe6d2d5a8ebb39c39e2440ce88c
Nile Jocson <atraphaxiae@gmail.com>
2026-08-20 21:12:39 +0800

    Use usize not u16 for disc/track indices

--- src/build.rs
+++ src/build.rs
@@ -26,8 +26,8 @@ impl Builder {
 
 #[derive(Debug, Serialize)]
 pub struct UnitTrack<'a> {
-	pub disc_index: u16,
-	pub track_index: u16,
+	pub disc_index: usize,
+	pub track_index: usize,
 	pub disc_info: &'a DiscInfo,
 	pub track_info: &'a TrackInfo,
 	pub file: PathBuf,
