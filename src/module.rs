4d4b3a49ea986b47ddfcb2d8c699c1ee972ed8d8
Nile Jocson <atraphaxiae@gmail.com>
2026-09-16 03:20:52 +0800

    Add manifest functions, run cargo fmt

--- src/module.rs
+++ src/module.rs
@@ -1,8 +1,8 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
-use std::{collections::HashMap, path::PathBuf};
 use serde::Deserialize;
+use std::{collections::HashMap, path::PathBuf};
 
 #[derive(Debug, Deserialize)]
 pub struct Module {
