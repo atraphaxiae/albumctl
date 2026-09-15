03f1ac547ed772a94cd85b859079c12463d07168
Nile Jocson <atraphaxiae@gmail.com>
2026-09-16 02:54:42 +0800

    Add cli structure

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
+use clap::Parser;
+
+use crate::cli::Cli;
+
+mod cli;
+
 fn main() {
-	println!("Hello, world!");
+	let args = Cli::parse();
 }
