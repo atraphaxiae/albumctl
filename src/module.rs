774fe67cbfa2c55032b758f58c9a987c6a58c2da
Nile Jocson <atraphaxiae@gmail.com>
2026-09-16 03:06:34 +0800

    Add serde with derive as dependency, make Module deserializable and its fields field flattened. Make Children untagged

--- src/module.rs
+++ src/module.rs
@@ -2,14 +2,17 @@
 // SPDX-License-Identifier: MPL-2.0
 
 use std::{collections::HashMap, path::PathBuf};
+use serde::Deserialize;
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct Module {
+	#[serde(flatten)]
 	fields: HashMap<String, String>,
 	children: Children,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
 enum Children {
 	Modules { modules: Vec<PathBuf> },
 	Files { files: Vec<PathBuf> },
