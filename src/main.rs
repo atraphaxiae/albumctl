c48465973642b8788fb4a1b2cdd3061df30da79e
Nile Jocson <atraphaxiae@gmail.com>
2026-09-15 20:21:03 +0800

    Prepare for a complete rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,6 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
-mod build;
-mod cli;
-mod command;
-mod dir;
-mod filesystem;
-mod manifest;
-mod model;
-mod print;
-mod result;
-mod run;
-mod source;
-
-use std::process::ExitCode;
-
-use error_stack::{Report, fmt::ColorMode};
-
-use crate::run::run;
-
-fn main() -> ExitCode {
-	Report::set_color_mode(ColorMode::Color);
-
-	if let Err(e) = run() {
-		eprintln!("{e:?}");
-		ExitCode::FAILURE
-	} else {
-		ExitCode::SUCCESS
-	}
+fn main() {
+	println!("Hello, world!");
 }
--- src/result.rs
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: Copyright (C) Nile Jocson <atraphaxiae@gmail.com>
-// SPDX-License-Identifier: MPL-2.0
-
-use std::result;
-
-use error_stack::Report;
-
-pub type Result<T, E> = result::Result<T, Report<E>>;
