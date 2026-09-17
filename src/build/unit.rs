b5a2440014d82b9f58ac026d51f936a08fdbf0ad
Nile Jocson <atraphaxiae@gmail.com>
2026-09-18 03:23:00 +0800

    Move validation of fields to very start of build_unit, fix some error messages

--- src/build/unit.rs
+++ src/build/unit.rs
@@ -25,14 +25,17 @@ pub fn build_unit(
 		dir: unit_dir.to_path_buf(),
 	};
 
-	let unit_build_dir = output_dir.join(format!(".albumctl/{}", hash));
-	ensure_dir(&unit_build_dir).change_context_lazy(error)?;
-
-	for file in files {
-		let from = unit_dir.join(&file.file);
-		let to = unit_build_dir.join(&file.file);
-		copy_file(&from, &to).change_context_lazy(error)?;
-	}
+	let artist = get_unit_field(metadata, unit_dir, "artist").change_context_lazy(error)?;
+	let year = get_unit_field(metadata, unit_dir, "year").change_context_lazy(error)?;
+	let album = get_unit_field(metadata, unit_dir, "album").change_context_lazy(error)?;
+	let release_year =
+		get_unit_field(metadata, unit_dir, "release_year").change_context_lazy(error)?;
+	let catalog_number =
+		get_unit_field(metadata, unit_dir, "catalog_number").change_context_lazy(error)?;
+	let media_type = get_unit_field(metadata, unit_dir, "media_type").change_context_lazy(error)?;
+	let audio_channels =
+		get_unit_field(metadata, unit_dir, "audio_channels").change_context_lazy(error)?;
+	let provenance = get_unit_field(metadata, unit_dir, "provenance").change_context_lazy(error)?;
 
 	let new_files = files
 		.iter()
@@ -52,23 +55,20 @@ pub fn build_unit(
 		.collect::<Result<Vec<_>, MetadataError>>()
 		.change_context_lazy(error)?;
 
+	let unit_build_dir = output_dir.join(format!(".albumctl/{}", hash));
+	ensure_dir(&unit_build_dir).change_context_lazy(error)?;
+
+	for file in files {
+		let from = unit_dir.join(&file.file);
+		let to = unit_build_dir.join(&file.file);
+		copy_file(&from, &to).change_context_lazy(error)?;
+	}
+
 	for (old_file, new_file) in files.iter().zip(new_files.iter()) {
 		let from = unit_build_dir.join(&old_file.file);
 		let to = unit_build_dir.join(&new_file.file);
 		move_file(&from, &to).change_context_lazy(error)?;
 	}
-
-	let artist = get_unit_field(metadata, unit_dir, "artist").change_context_lazy(error)?;
-	let year = get_unit_field(metadata, unit_dir, "year").change_context_lazy(error)?;
-	let album = get_unit_field(metadata, unit_dir, "album").change_context_lazy(error)?;
-	let release_year =
-		get_unit_field(metadata, unit_dir, "release_year").change_context_lazy(error)?;
-	let catalog_number =
-		get_unit_field(metadata, unit_dir, "catalog_number").change_context_lazy(error)?;
-	let media_type = get_unit_field(metadata, unit_dir, "media_type").change_context_lazy(error)?;
-	let audio_channels =
-		get_unit_field(metadata, unit_dir, "audio_channels").change_context_lazy(error)?;
-	let provenance = get_unit_field(metadata, unit_dir, "provenance").change_context_lazy(error)?;
 
 	let unit_output_dir = output_dir.join(format!(
 		"{artist} - ({year}) {album}/({release_year}) {catalog_number} [{media_type}, {audio_channels}, {provenance}]"
@@ -124,14 +124,14 @@ fn get_unit_field<'a>(
 
 #[derive(Debug, Error)]
 pub enum MetadataError {
-	#[error("Track {disc_number}.{track_number:02} is missing field \"{field}\"")]
+	#[error("Track {disc_number}.{track_number:02} is missing field '{field}'")]
 	TrackFieldMissing {
 		disc_number: usize,
 		track_number: usize,
 		field: String,
 	},
 
-	#[error("Unit {dir} is missing field \"{field}\"")]
+	#[error("Unit {dir:?} is missing field '{field}'")]
 	UnitFieldMissing { dir: PathBuf, field: String },
 }
 
