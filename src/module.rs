37c0b55e276d6a5e827d89f93eaaa5b12f6b6897
Nile Jocson <atraphaxiae@gmail.com>
2026-09-18 22:35:20 +0800

    Add Conversion and Replaygain structs

--- src/module.rs
+++ src/module.rs
@@ -7,8 +7,34 @@ use std::{collections::BTreeMap, path::PathBuf};
 #[derive(Debug, Deserialize)]
 pub struct RootModule {
 	pub output_directory: PathBuf,
+	pub ffmpeg_command: Option<String>,
+	pub rsgain_command: Option<String>,
+	pub conversion: Option<Conversion>,
+	pub replaygain: Option<Replaygain>,
 	pub metadata: Metadata,
 	pub children: RootChildren,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Conversion {
+	pub target_format: String,
+	pub sample_rate: u32,
+	pub sample_format: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Replaygain {
+	pub album_gain: bool,
+	pub target_lufs: f32,
+	pub clip_mode: ClipMode,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ClipMode {
+	Disabled,
+	PositiveGain,
+	AlwaysEnabled,
 }
 
 #[derive(Debug, Deserialize)]
